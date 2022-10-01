a55a02dc613e79f2d6babbb76dd2c0d13dbf2d0e
Bachir <bachirmdicko@gmail.com>
2022-10-01 12:59:57 -0400

    Refactored code for Efficiency

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import {useContext} from 'react'
+import { SearchContext } from '../context/SearchContext';
 
-function SearchBar (props) {
-    let [searchTerm, setSearchTerm] = useState('')
+function SearchBar () {
+    const {term, handleSearch} = useContext(SearchContext)
 
     return (
         <div>
             <form>
-                <input type="text" placeholder="Search Here" onChange={(e) => props.handleSearch(e, e.target.value)} />
-                <input type="submit" />
+                <input type="text" placeholder="Search Here" ref={term} onChange={(e) => handleSearch(e, term.current.value)} />
+                <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
             </form>
         </div>
     )
